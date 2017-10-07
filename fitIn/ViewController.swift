32896e6c564866583e0768332fb5f56553147d98
Avery Jones <averyj@sfu.ca>
2017-10-06 17:53:23 -0700

    Initial Commit

---
+++ fitIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fitIn
+//
+//  Created by Avery Jones on 2017-10-06.
+//  Copyright © 2017 AJ productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
