96fae8710ce1cdd7ee6d748b41a632915bdf153c
Avery Jones <averyj@sfu.ca>
2017-10-24 15:45:50 -0700

    Updated situation class. Included responseType enum to easily sort the expected type of response the question is asking.
    Currently responseType uses associated values ( when you declare a var of your enum type, you can give it something called associatedValues, these can be access when using a switch statement, or by the methods defined in the enum)

--- fitIn/situation.swift
+++ fitIn/situation.swift
@@ -9,21 +9,74 @@
 import Foundation
 
 struct situation {
-
+    
+    // Enum for the type of response that the situation requires
+    // MAY potentially use associated values for storing of answer type
+    enum responseType{
+        case yesOrNo(Bool)
+        case slider(Double)
+        case multipleChoice(Int)
+        
+        func getBool() -> Bool? {
+            switch self{
+            case .yesOrNo(let value):
+                return value
+            default:
+                return nil
+            }
+        }
+        func getNumber() -> Double? {
+            switch self {
+            case .slider(let value):
+                return value
+            case .multipleChoice(let value):
+                return Double(value)
+            default:
+                return nil
+            }
+        }
+        
+    }
+    var inputAnswer : responseType?
+    
     //MARK: VARIABLES
     var situationTags = [String]() // List of metadata / situation Tags
     // just a random imgur url for initialization, Needs init func
     private var imageLoc : URL
-    private var rightAnswer : Bool
+    //private var rightAnswer : Bool
+    private var response : responseType
 
     //MARK: METHODS
-    init(situationID: String){
+    init(situationID: String, type : responseType){
+        // Insert
         imageLoc = URL(string: "https:i.imgur.com/I8wCreu.jpg")!
-        rightAnswer = false
+        response = type
     }
     
-    func isProSocial() -> Bool {
-        return rightAnswer
+    func isProSocial() -> Bool? {
+        return response.getBool()
+    }
+    func isRightAnswer() -> Bool {
+        if let answer = inputAnswer {
+        switch answer {
+        case .yesOrNo(let value):
+            let expectedResponse = response.getBool()
+            if value == expectedResponse {
+                return true
+            }
+            return false
+        case .multipleChoice(let value):
+            let expectedResponse = Int(response.getNumber()!)
+            if value == expectedResponse {
+                return true
+            }
+            return false
+        case .slider(let value):
+            let expectedResponse = response.getNumber()
+            if value == expectedResponse {
+                return true
+            }
+            return false
     }
 
     func getImageData() -> Data { // Get Image Data from URL
