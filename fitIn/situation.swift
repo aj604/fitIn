65a3f041a02e97a96ca66f1bba2a69969c7e25ea
Avery Jones <averyj@sfu.ca>
2017-10-24 16:04:16 -0700

    Cleaned up code, Added more clear response return functions

--- fitIn/situation.swift
+++ fitIn/situation.swift
@@ -25,61 +25,74 @@ struct situation {
                 return nil
             }
         }
-        func getNumber() -> Double? {
+        func getSlider() -> Double? {
             switch self {
             case .slider(let value):
                 return value
-            case .multipleChoice(let value):
-                return Double(value)
             default:
                 return nil
             }
         }
-        
+        func getMultipleChoice() -> Int? {
+            switch self {
+            case .multipleChoice(let value):
+                return value
+            default:
+                return nil
+            }
+        }
     }
-    var inputAnswer : responseType?
     
     //MARK: VARIABLES
+    var inputAnswer : responseType?
     var situationTags = [String]() // List of metadata / situation Tags
     // just a random imgur url for initialization, Needs init func
     private var imageLoc : URL
-    //private var rightAnswer : Bool
-    private var response : responseType
-
+    private var response : responseType // Includes both the type of response and the answer
+    private var tipsForNextTime : String
+    
+    
     //MARK: METHODS
+
+    // Need to make this a load function from our DB based upon situation ID
+    // Will init the imageURL either locally or from URL from db
     init(situationID: String, type : responseType){
-        // Insert
         imageLoc = URL(string: "https:i.imgur.com/I8wCreu.jpg")!
         response = type
+        tipsForNextTime = "Sucks to suck"
     }
     
-    func isProSocial() -> Bool? {
-        return response.getBool()
-    }
-    func isRightAnswer() -> Bool {
+    // general answer checking method
+    // Pre: Situation is loaded and inputAnswer != nil
+    // Post: Bool? determining if they got the right answer or if inputAnswer wasnt initialized
+    private func isRightAnswer() -> Bool? {
         if let answer = inputAnswer {
-        switch answer {
-        case .yesOrNo(let value):
-            let expectedResponse = response.getBool()
-            if value == expectedResponse {
-                return true
+            switch answer {
+            case .yesOrNo(let value):
+                let expectedResponse = response.getBool()
+                if value == expectedResponse {
+                    return true
+                }
+                return false
+            case .multipleChoice(let value):
+                let expectedResponse = response.getMultipleChoice()
+                if value == expectedResponse {
+                    return true
+                }
+                return false
+            case .slider(let value):
+                let expectedResponse = response.getSlider()
+                if value == expectedResponse {
+                    return true
+                }
+                return false
             }
-            return false
-        case .multipleChoice(let value):
-            let expectedResponse = Int(response.getNumber()!)
-            if value == expectedResponse {
-                return true
-            }
-            return false
-        case .slider(let value):
-            let expectedResponse = response.getNumber()
-            if value == expectedResponse {
-                return true
-            }
-            return false
+        }
+        print("Code should NEVER get here, This means the response type and answer was not initialized for this situation ")
+        return nil
     }
-
-    func getImageData() -> Data { // Get Image Data from URL
+    
+    func getImageData() -> Data { // Get Image Data from URL / Local
         var imageOut = Data()//Data type, to prep image for UIImageView
         do{
             try imageOut = Data(contentsOf: imageLoc) //Primary image location
@@ -88,7 +101,7 @@ struct situation {
                 // backup local failedToLoad resource. Should not fail... needs work
                 try imageOut = Data(contentsOf: Bundle.main.url(forResource: "failedToLoad", withExtension: ".png")!)
             } catch {
-            
+                print("UHOH Cant find our local failed to load image!")
             }
         }
         return imageOut
