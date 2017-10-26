0ce37f913898c774bfa76c17f1309ffca597457e
Avery Jones <averyj@sfu.ca>
2017-10-25 17:04:42 -0700

    Beginning to write new view controller for the different response types

---
+++ fitIn/responseViewController.swift
@@ -0,0 +1,39 @@
+//
+//  responseViewController.swift
+//  fitIn
+//
+//  Created by Avery Jones on 2017-10-25.
+//  Copyright © 2017 AJ productions. All rights reserved.
+//
+
+import UIKit
+
+class responseViewController: UIViewController {
+    @IBAction func picIsAntiSocial(_ sender: Any) {
+    }
+    @IBAction func picIsProSocial(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
