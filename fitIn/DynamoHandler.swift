ba91f873073552ae098b7abb776f03e5f6dd843d
schecko <schecko@sfu.ca>
2017-10-30 17:44:44 -0700

    pulled changes from other branch to add dynamoHandler

---
+++ fitIn/DynamoHandler.swift
@@ -0,0 +1,59 @@
+//
+//  DynamoHandler.swift
+//  fitIn
+//
+//  Created by schecko on 10/30/17.
+//  Copyright © 2017 AJ productions. All rights reserved.
+//
+
+import Foundation
+import AWSDynamoDB
+
+class Scenario2 : AWSDynamoDBObjectModel, AWSDynamoDBModeling {
+    
+    var scenarioID: Int
+    
+    init() {
+        self.scenarioID = 0;
+    }
+    
+    static func dynamoDBTableName() -> String {
+        return SCENARIO_MASTER_TABLE
+    }
+    
+    static func hashKeyAttribute() -> String {
+        return SCENARIO_MASTER_TABLE_PRIMARY_KEY
+    }
+
+}
+
+class DynamoHandler {
+    var paginatedOutput: AWSDynamoDBPaginatedOutput?
+    var dynamo: AWSDynamoDBObjectMapper
+    
+    init() {
+        do {
+            dynamo = AWSDynamoDBObjectMapper.default();
+        } catch {
+            print("Fuck")
+        
+        }
+    }
+    
+    func putItem(scenario : Scenario2)  {
+        dynamo
+            .save(scenario as AWSDynamoDBObjectModel & AWSDynamoDBModeling)
+            .continueWith(block:
+                { (task:AWSTask<AnyObject>!) -> Any? in
+                                        if let error = task.error {
+                                            print("The request failed. Error: \(error)")
+                                        } else {
+                                            print("success")
+                                            // Do something with task.result or perform other operations.
+                                            
+                                        }
+                                        return task.result
+            })
+    }
+    
+}
