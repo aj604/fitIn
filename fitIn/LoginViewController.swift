e6988198bf225a37669e4d19b186cbae30e11576
Kevin C <kevinc@Kevins-iMac.local>
2017-11-03 11:38:32 -0700

    adding back in login controller

--- fitIn/LoginViewController.swift
+++ fitIn/LoginViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class LoginViewController: UIViewController {
     
     //Variables
-    var currentuser = userProfile.current()
+    var currentuser = UserProfile.current()
     var password: String = ""
     
     //Outlets
