d64a9489a475890b0295a295bcef61662afc7a27
Vlad Polin <vpolin@asb9700-e02.csil.sfu.ca>
2017-11-02 20:39:18 -0700

    scott issues

---
+++ fitIn/SupplementaryFiles/aws-resources (2).swift
@@ -0,0 +1,19 @@
+//
+//  aws-resources.swift
+//  fitIn
+//
+//  Created by schecko on 10/26/17.
+//  Copyright © 2017 AJ productions. All rights reserved.
+//
+
+import Foundation
+
+let AWS_IDENTITY_POOL = "us-west-2:3780c55d-22e7-48a5-b63d-763bb6b479a6"
+
+// scenario master
+let SCENARIO_MASTER_TABLE = "scenarioMaster"
+let SCENARIO_MASTER_TABLE_PRIMARY_KEY = "scenarioID"
+
+// user profiles
+let USER_PROFILES_TABLE = "userProfiles"
+let USER_PROFILES_TABLE_PRIMARY_KEY = "emailAddress"
