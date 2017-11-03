71432bd49905afba1762eac3f4c9e2ba66716a61
Avery Jones <averyj@sfu.ca>
2017-11-03 14:31:20 -0700

    Included new views and their controllers. Need to do segue and preparation

--- fitIn/MultipleChoiceViewController.swift
+++ fitIn/MultipleChoiceViewController.swift
@@ -9,27 +9,60 @@
 import UIKit
 
 class MultipleChoiceViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    //Control Current Scenario
+    private var scenarioController = ScenarioHandler()
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scenarioDescription: UILabel!
+    
+    
+    // Use these outlets to set the button text
+    @IBOutlet weak var optionA: UIButton!
+    @IBOutlet weak var optionB: UIButton!
+    @IBOutlet weak var optionC: UIButton!
+    @IBOutlet weak var optionD: UIButton!
+    
+    @IBAction func choiceA(_ sender: UIButton) {
+        scenarioController.voteChoice = 0
+        scenarioController.loadNextScenario()
+        updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func choiceB(_ sender: UIButton) {
+        scenarioController.voteChoice = 1
+        scenarioController.loadNextScenario()
+        updateUI()
+    }
+    @IBAction func choiceC(_ sender: UIButton) {
+        scenarioController.voteChoice = 2
+        scenarioController.loadNextScenario()
+        updateUI()
+    }
+    @IBAction func choiceD(_ sender: UIButton) {
+        scenarioController.voteChoice = 3
+        scenarioController.loadNextScenario()
+        updateUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Update the UI to represent the change in Scenario
+    // Once different response views are set they can be set here
+    func updateUI() {
+        if let image = UIImage(data: scenarioController.loadScenarioImageData()){
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+        }
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Try loading image data from first Scenario
+        if let image = UIImage(data: scenarioController.loadScenarioImageData()){
+            print("image loaded")
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+        } else {
+            print("hmm something went wrong buffering the initial image")
+        }
+    }
 }
--- fitIn/SliderViewController.swift
+++ fitIn/SliderViewController.swift
@@ -9,27 +9,47 @@
 import UIKit
 
 class SliderViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scenarioDescription: UILabel!
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    //Control Current Scenario
+    private var scenarioController = ScenarioHandler()
+    
+    //User input
+    @IBAction func submit(_ sender: UIButton) {
+        scenarioController.voteChoice = Int(sliderValue.value)
+        scenarioController.loadNextScenario()
+        updateUI()
+    }
+    
+    // Update the UI to represent the change in Scenario
+    // Once different response views are set they can be set here
+    func updateUI() {
+        if let image = UIImage(data: scenarioController.loadScenarioImageData()){
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Try loading image data from first Scenario
+        if let image = UIImage(data: scenarioController.loadScenarioImageData()){
+            print("image loaded")
+            imageView.contentMode = .scaleAspectFit
+            imageView.image = image
+        } else {
+            print("hmm something went wrong buffering the initial image")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
