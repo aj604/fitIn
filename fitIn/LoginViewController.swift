bbb6af6dd402166fe1d4ee5ef63752129afd0200
Vlad Polin <vpolin@asb9700-f02.csil.sfu.ca>
2017-11-03 22:41:51 -0700

    added login functionality, this works but not from a UI perspective

--- fitIn/LoginViewController.swift
+++ fitIn/LoginViewController.swift
@@ -12,7 +12,7 @@ class LoginViewController: UIViewController {
     
     //Variables
     var currentuser = UserProfile.current()
-    var password: String = ""
+    //var password: String = ""
     
     //Outlets
     @IBOutlet weak var EmailTextField: UITextField!
@@ -39,11 +39,49 @@ class LoginViewController: UIViewController {
     
     @IBAction func Login(_ sender: UIButton) {
         
-        currentuser!.emailAddress = EmailTextField.text!
-        password = PasswordFromLoginScreen.text!
+        let email = EmailTextField.text!
+        let password = PasswordFromLoginScreen.text!
+        guard let currentUser = UserProfile.current() else { return }
         
-        print(currentuser!.emailAddress, password)
-        
+        dynamoHandler
+            .getUserProfile(email)
+            .continueWith(block:
+                { (task) -> Void in
+                    if(task.result == nil)
+                    {
+                        currentUser.isUserLoggedIn = false
+                        print("user does not exist in database")
+                        return
+                    }
+                    else if (task.result != nil && task.result!.passwordToken == password)
+                    {
+                        currentUser.isUserLoggedIn = true
+                        print("user has successfully logged in")
+                        currentUser.emailAddress = task.result!.emailAddress
+                        currentUser.userName = task.result!.userName
+                        currentUser.userAge = task.result!.userAge
+                        currentUser.userLifetime = task.result!.userLifetime
+                        currentUser.numScenariosAnswered = task.result!.numScenariosAnswered
+                        currentUser.numScenariosCorrect = task.result!.numScenariosCorrect
+                        currentUser.averageResponseTime = task.result!.averageResponseTime
+                        return
+                    }
+                    else
+                    {
+                        currentUser.isUserLoggedIn = false
+                        print("user has failed to login")
+                        return
+                    }
+            })
+        //
+        //The issue is that you cannot call modifications to text fields from the dynamo handler tasks, since they do not use the main thread
+        //but by the time the dynamo task finishes the main thread attempts to evaluated the below statement and is not able to set
+        //currently logging in and signing up works, but we have no UI to show this
+        //
+        /*if (didFinish == true && currentUser.isUserLoggedIn == false) {
+            self.EmailTextField.textColor = UIColor.red
+            self.PasswordFromLoginScreen.textColor = UIColor.red
+        }*/
     }
     
     
