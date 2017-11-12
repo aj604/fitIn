81397ab4d32e423e60d19067342af5966264dbf4
Scott Checko <scott.checko@hotmail.com>
2017-11-11 22:30:36 -0800

    better than swift

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 // This is the "main" file used by lambda, lambda will import it and attempt to call the handler function
 import * as AWS from "aws-sdk";
 
+AWS.config.update({region: "us-west-2"});
 let dynamo = new AWS.DynamoDB();
 
 console.log("Loading function");
@@ -12,5 +13,21 @@ exports.handler = (event, context, callback) => {
         console.log(record.eventName);
         console.log('DynamoDB Record: %j', record.dynamodb);
     });*/
-    callback(null, "Success");
+
+    let scanArgs: AWS.DynamoDB.ScanInput = {
+        TableName: "scenarioMaster",
+        ReturnConsumedCapacity: "TOTAL",
+        ConsistentRead: false
+    }
+
+    dynamo.scan(scanArgs).promise()
+    .then((result: AWS.DynamoDB.ScanOutput) => {
+        console.log(result);
+        console.log("done");
+
+
+        callback(null, "Success");
+    })
+
+
 };
