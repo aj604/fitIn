ab60d0bbf22fddf3e63cda0d9888c9028056d3d8
Scott Checko <scott.checko@hotmail.com>
2017-11-11 23:26:43 -0800

    cleaning up a bit

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,14 +8,9 @@ console.log("Loading function");
 
 exports.handler = (event, context, callback) => {
     console.log("Received event:", JSON.stringify(event, null, 2));
-    /*event.Records.forEach((record) => {
-        console.log(record.eventID);
-        console.log(record.eventName);
-        console.log('DynamoDB Record: %j', record.dynamodb);
-    });*/
 
     let scanArgs: AWS.DynamoDB.ScanInput = {
-        TableName: "scenarioMaster",
+        TableName: "scenarioUpdate",
         ReturnConsumedCapacity: "TOTAL",
         ConsistentRead: false
     }
