f13a197a24cf1cbd48f4f11831f7bf0301465c81
Avery Jones <averyj@sfu.ca>
2017-11-18 21:50:45 -0800

    Adds skeleton of ScenarioTableView and ScenarioTableVIewCell

---
+++ client/fitIn/Views/ScenarioTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ScenarioTableViewCell.swift
+//  
+//
+//  Created by Avery Jones on 2017-11-18.
+//
+
+import UIKit
+
+class ScenarioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
