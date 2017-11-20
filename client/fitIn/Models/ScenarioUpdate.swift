86c1a295b91126e7f91757746ddba64269be8d9d
Avery Jones <averyj@sfu.ca>
2017-11-19 16:12:01 -0800

    mess

---
+++ client/fitIn/Models/ScenarioUpdate.swift
@@ -0,0 +1,57 @@
+//
+//  ScenarioUpdate.swift
+//  fitIn
+//
+//  Created by schecko on 11/17/17.
+//  Copyright © 2017 group of 5. All rights reserved.
+//
+
+import Foundation
+import AWSDynamoDB
+
+class ScenarioUpdate {
+    
+    var scenarioID : String
+    var updateID : String
+    var answeredBy : String
+    var userAnswer : Int
+    var timeToAnswer: Int // in milliseconds
+    
+    
+    required init(scenarioID: String, userAnswer: Int) {
+        self.scenarioID = scenarioID;
+        updateID = String(arc4random());
+        if let email = UserProfile.current()?.emailAddress {
+            answeredBy = email;
+        } else {
+            answeredBy = "Anonymous";
+        }
+        self.userAnswer = userAnswer;
+        timeToAnswer = 0;
+    }
+    
+    required init() {
+        scenarioID = String(arc4random());
+        updateID = String(arc4random());
+        if let email = UserProfile.current()?.emailAddress {
+            answeredBy = email;
+        } else {
+            answeredBy = "Anonymous";
+        }
+        self.userAnswer = 0;
+        timeToAnswer = 0;
+    }
+    
+    // Creates and returns a DynamoDB compatible dictionary representing this class.
+    func toDBDictionary() -> [String : AWSDynamoDBAttributeValue] {
+        
+        return [
+            SCENARIO_MASTER_TABLE_PRIMARY_KEY: makeAttrib(self.scenarioID),
+            "updateID": makeAttrib(self.updateID),
+            "answeredBy": makeAttrib(self.answeredBy),
+            
+            "userAnswer": makeAttrib(self.userAnswer),
+            "timeToAnswer": makeAttrib(self.timeToAnswer)
+        ]
+    }
+}
